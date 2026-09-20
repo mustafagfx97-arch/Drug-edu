1b748efb176c6649e9367d32dac8c3a27abe7839
mustafagfx97-arch <mustafagfx97@gmail.com>
2026-09-20 19:45:53 +0300

    feat: add supplement_profile.dart

---
+++ lib/features/supplements/domain/supplement_profile.dart
@@ -0,0 +1,29 @@
+import '../../../core/models/medication.dart';
+
+class SupplementProfile {
+  const SupplementProfile({
+    required this.id,
+    required this.group,
+    required this.name,
+    required this.subtitle,
+    required this.formulation,
+    required this.howToTakeEn,
+    required this.pharmacistSections,
+    required this.patient,
+    required this.sourceLabel,
+    this.formulationAlert = '',
+    this.isPediatric = false,
+  });
+
+  final String id;
+  final String group;
+  final String name;
+  final String subtitle;
+  final String formulation;
+  final String howToTakeEn;
+  final String formulationAlert;
+  final List<MedicationSection> pharmacistSections;
+  final PatientCounselingData patient;
+  final String sourceLabel;
+  final bool isPediatric;
+}
